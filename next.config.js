2506ab638be62b3cb78fe76797718015088d4db3
Solomon Wislon <delmax.sw@gmail.com>
2025-06-19 10:50:43 +0000

    Updated codebase with improve UI/UX, interactive maps

--- next.config.js
+++ next.config.js
@@ -3,14 +3,10 @@
 const nextConfig = {
     images: {
         domains: ['images.unsplash.com'],
-<<<<<<< HEAD
-    }
-=======
     },
     env: {
         NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
     },
->>>>>>> 049e20b (Updated codebase with improve UI/UX, interactive maps)
 };
 
 if (process.env.NEXT_PUBLIC_TEMPO) {
@@ -18,10 +14,8 @@ if (process.env.NEXT_PUBLIC_TEMPO) {
         // NextJS 13.4.8 up to 14.1.3:
         // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
         // NextJS 14.1.3 to 14.2.11:
-        swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
-
-        // NextJS 15+ (Not yet supported, coming soon)
-    }
+        swcPlugins: [["tempo-devtools/swc/0.86", {}]],
+    };
 }
 
 module.exports = nextConfig;
